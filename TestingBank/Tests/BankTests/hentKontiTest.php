c4b9934d055de793d8e21db37b9d24777fc20e8d
einarsolbakken <43601639+einarsolbakken@users.noreply.github.com>
2020-03-06 12:59:24 +0100

    Merge branch 'master' of /Users/einarsolbakken/Documents/GitHub/TestingBank/TestingBank/TestingBank with conflicts.

--- TestingBank/Tests/BankTests/hentKontiTest.php
+++ TestingBank/Tests/BankTests/hentKontiTest.php
@@ -4,36 +4,27 @@ include_once '../../DAL/bankDatabaseStub.php';
 include_once '../../BLL/bankLogikk.php';
 
 class hentKontiTest extends PHPUnit\Framework\TestCase {
-    public function test_hentKonti()
-    {
+    /*HERRO*/
+ public function test_hentKonti()
+ {
      //arrange
-     $personnummer = "01010122344"; // Korrekt personnummer
-     $bankLogikk = new Bank(new BankDBStub());
-
+     $personnummer="12345654323";
+     $bankLogikk=new Bank(new BankDBStub());
      //act
-     $allKonti = $bankLogikk->hentKonti($personnummer);
-
+     $OK=$bankLogikk->hentKonti($personnummer);
      //assert
-     $this->assertEquals("98765432123", $allKonti[0]);
-     $this->assertEquals("23456543456", $allKonti[1]);
-     echo "Konti nr 1: ".$allKonti[0];
-     echo "\n";
-     echo "Konti nr 2: ".$allKonti[1];
+     $this->assertEquals(1,count($OK));
     }
-
-    public function test_hentKontiFeilPersonummer()
-    {
+ 
+    /*public function testhentKontiPersonnrFeil(){
         //arrange
-        $personnummer = "01010122355"; // Feil personnummer
-        $bankLogikk = new Bank(new BankDBStub());
-
+        $personnummer = 0000000000; 
+        $bank=new Bank(new BankDBStub());
         //act
-        $allKonti = $bankLogikk->hentKonti($personnummer);
-
+        $konto = $bank->hentKonti($personnummer);
         //assert
-        $this->assertEquals(0,count($allKonti));
-        echo "Feil personnummer gir tomt array";
-
-    }
+        $this->assertEquals(" ",$konto[0]);
+    }*/
+    
 }
 ?>
