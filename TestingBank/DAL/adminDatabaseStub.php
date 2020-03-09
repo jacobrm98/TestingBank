f37cf9155fbd4af35a0f23d6e029c1249bf38bb5
Jacob Mangelrod <jacobmangelrod@gmail.com>
2020-03-09 16:44:30 +0100

    Skrevet adminDatabaseStub med kommentarer. Forventer vi går gjennom adminDatabaseStub. Har også konmmentert hvilken funksjoner/metoder som er hos bankDatabase og bankDatabaseStub som IKKE finnes/tilhører til bankLogikk

--- TestingBank/DAL/adminDatabaseStub.php
+++ TestingBank/DAL/adminDatabaseStub.php
@@ -3,10 +3,10 @@ include_once '../Model/domeneModell.php';
 
 class AdminDBStub
 {
-
-    function hentAlleKunder()
+function hentAlleKunder()
     {
         $alleKunder = array();
+        
         $kunde1 = new kunde();
         $kunde1->personnummer = "01010122344";
         $kunde1->navn = "Per Olsen";
@@ -27,6 +27,7 @@ class AdminDBStub
         $kunde3->adresse = "Bærumsveien 23, 1234 Bærum";
         $kunde3->telefonnr = "99889988";
         $alleKunder[] = $kunde3;
+        
         return $alleKunder;
     }
     function endreKundeInfo($kunde)
@@ -39,6 +40,7 @@ class AdminDBStub
         $kunde1->telefonnr = "12345678";
         $kunde1->passord = "HeiHei";
         $kunde1->personnummer = "12345678901";
+        
         if($kunde->fornavn == $kunde1->fornavn && $kunde->etternavn == $kunde1->etternavn)
         {
             return "OK";
@@ -49,7 +51,7 @@ class AdminDBStub
         }
 
     }
-    function registrerKunde($kunde)
+    /*function registrerKunde($kunde) Koden til Einar (shit kode)
     {
         if($kunde->postnr == "0481")
         {
@@ -60,10 +62,41 @@ class AdminDBStub
             return "Feil";
         }
 
+    }*/
+    function registrerKunde($kunde)
+    
+//Usikker om man skal ha personnummeret, men Krattebøl har brukt ID-nummer på en lik funksjon på Enhetstesting
+    {
+        $kunde1 = new kunde();
+        $kunde1->fornavn = "Ola";
+        $kunde1->etternavn = "Nordmann";
+        $kunde1->adresse = "Heiaveien 5";
+        $kunde1->postnr = "0477";
+        $kunde1->telefonnr = "12345678";
+        $kunde1->passord = "HeiHei";
+        $kunde1->personnummer = "12345678901";
+        
+        if($kunde1->personnummer == $kunde->personnummer)
+        {
+            return "OK";
+        }
+        else
+        {
+            return "Feil";
+        }
     }
     function slettKunde($personnummer)
     {
-        if($personnummer == 1)
+        $kunde1 = new kunde();
+        $kunde1->fornavn = "Ola";
+        $kunde1->etternavn = "Nordmann";
+        $kunde1->adresse = "Heiaveien 5";
+        $kunde1->postnr = "0477";
+        $kunde1->telefonnr = "12345678";
+        $kunde1->passord = "HeiHei";
+        $kunde1->personnummer = "12345678901";
+        
+        if($kunde1->personnummer == $personnummer)
         {
             return "OK";
         }
@@ -73,13 +106,42 @@ class AdminDBStub
         }
 
     }
-    function registrerKonto($kunde)
+    function registrerKonto($konto) 
     {
+        $konti1 = new konto();
+        $konti1->personnummer = "01010122344";
+        $konti1->kontonummer = "98765432123";
+        $konti1->saldo = 2000;
+        $konti1->type = "Brukskonto";
+        $konti1->valuta = "NOK";
+        
+        if($konti1->personnummer == $kunde->personnummer)
+        {
+            return "OK";
+        }
+        else
+        {
+            return "Feil";
+        }
 
     }
     function endreKonto($konto)
     {
-
+        $konti1 = new konto();
+        $konti1->personnummer = "01010122344";
+        $konti1->kontonummer = "98765432123";
+        $konti1->saldo = 2000;
+        $konti1->type = "Brukskonto";
+        $konti1->valuta = "NOK";
+        
+        if($konti1->personnummer == $konto->personnummer)//Søker etter personnummeret til konto på adminDatabase.php
+        {
+            return "OK";
+        }
+        else
+        {
+            return "Feil";
+        }
     }
 
     function hentAlleKonti()
@@ -100,15 +162,34 @@ class AdminDBStub
         $konti2->type = "Sparekonto";
         $konti2->valuta = "NOK";
 
-        if($personnummer == $konti1->personnummer)
+        if($personnummer == $konti1->personnummer)//I adminDatabase.php så henter den all info fra konto. Ikke bare kontonummer
         {
-            $allKonti[] = $konti1->kontonummer;
-            $allKonti[] = $konti2->kontonummer;
+            $allKonti[] = $konti1;//Stod $konti1->kontonummer
+            $allKonti[] = $konti2;//Stod $konti2->kontonummer
         }
         return $allKonti;
     }
     function slettKonto($kontonummer)
     {
-
+        $kunde1 = new kunde();
+        $kunde1->fornavn = "Ola";
+        $kunde1->etternavn = "Nordmann";
+        $kunde1->adresse = "Heiaveien 5";
+        $kunde1->postnr = "0477";
+        $kunde1->telefonnr = "12345678";
+        $kunde1->passord = "HeiHei";
+        $kunde1->personnummer = "12345678901";
+        
+        if($kunde->kontonummer == $kontonummer)
+        {
+            return "OK";
+        }
+        else
+        {
+            return "Feil i personnummer";
+//Usikker om man har bruker "Feil" eller "Feil i personnummer". 
+//I adminDatabase.php så brukes det json_encode med "Feil i personnummer".
+        }
     }
-}
+}  
+?>
