b8c2e88d68fffd59c756ae550886cd914533c2ba
Jacob Mangelrod <jacobmangelrod@gmail.com>
2020-03-09 16:59:38 +0100

    Takk for idag, så helt feil. Gjorde om endringene

--- TestingBank/DAL/adminDatabaseStub.php
+++ TestingBank/DAL/adminDatabaseStub.php
@@ -169,7 +169,7 @@ function hentAlleKunder()
         }
         return $allKonti;
     }
-    function slettKonto($personnummer)
+    function slettKonto($kontonummer)
     {
         $kunde1 = new kunde();
         $kunde1->fornavn = "Ola";
@@ -180,7 +180,7 @@ function hentAlleKunder()
         $kunde1->passord = "HeiHei";
         $kunde1->personnummer = "12345678901";
         
-        if($kunde->personnummer == $personnummer)
+        if($kunde->kontonummer == $kontonummer)
         {
             return "OK";
         }
